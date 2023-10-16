b99a7048dc2cb982ba16ff155d49db959cbe3977
modii <modiibeats@gmail.com>
2023-10-16 15:28:22 +0100

    add ability handler

---
+++ Assets/Scripts/Abilities/AbilityHandler.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityHandler : MonoBehaviour
+{
+    public Ability ability;
+    float cooldownTimer;
+    float activeTimer;
+
+    enum AbilityState
+    {
+        Ready,
+        Active,
+        Cooldown,
+    }
+
+    AbilityState state;
+
+    KeyCode key;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state) {
+            case AbilityState.Ready:
+                if (Input.GetKeyDown(key))
+                {
+                    ability.Activate();
+                }
+                break;
+            case AbilityState.Active:
+                if (activeTimer > 0)
+                {
+                    activeTimer -= Time.deltaTime;
+                }
+                else
+                {
+                    state = AbilityState.Cooldown;
+                    cooldownTimer = ability.cooldown;
+                }
+                break;
+            case AbilityState.Cooldown:
+                if (cooldownTimer > 0)
+                {
+                    cooldownTimer -= Time.deltaTime;
+                }
+                else
+                {
+                    state = AbilityState.Ready;
+                }
+                break;
+
+        }
+    }
+}
