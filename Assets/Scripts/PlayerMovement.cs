e956cef900c248abef3fe592ed989820fc2e8d91
modii <modiibeats@gmail.com>
2023-10-17 12:00:36 +0100

    replace old flip logic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -330,9 +330,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Flip()
     {
-        Vector3 scale = transform.localScale;
-        scale.x *= -1;
-        transform.localScale = scale;
+        transform.Rotate(0f, 180f, 0f);
         isFacingRight = !isFacingRight;
     }
 
