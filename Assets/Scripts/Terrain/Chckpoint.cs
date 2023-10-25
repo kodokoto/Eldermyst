536e0080e9610e4fbb1d94dedb5529bd1a173484
modii <modiibeats@gmail.com>
2023-10-25 14:30:08 +0100

    add checkpoint

---
+++ Assets/Scripts/Terrain/Chckpoint.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Chckpoint : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameManager.instance.SetGameState(GameState.Won);
+        }
+    }
+}
