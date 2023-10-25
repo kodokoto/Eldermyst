5c9963023adf1372279aaa2daf8d3bc5cb5b3c3c
modii <modiibeats@gmail.com>
2023-10-25 14:03:21 +0100

    added UI manager

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+// union type for screens
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+    
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    public void ShowGameOverScreen()
+    {
+        Debug.Log("Showing game over screen");
+        gameObject.transform.Find("GameOverScreen").gameObject.SetActive(true);
+    }
+
+    public void HideGameOverScreen()
+    {
+        gameObject.transform.Find("GameOverScreen").gameObject.SetActive(false);
+    }
+
+    public void ShowWinScreen()
+    {
+        gameObject.transform.Find("WinScreen").gameObject.SetActive(true);
+    }
+
+    public void HideWinScreen()
+    {
+        gameObject.transform.Find("WinScreen").gameObject.SetActive(false);
+    }
+}
