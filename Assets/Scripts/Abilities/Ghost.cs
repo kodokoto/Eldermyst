5c4c981dd47a4a90e58da8066a9c497cafad9ab5
jjasici1 <ec21744@qmul.ac.uk>
2023-12-12 17:03:25 +0000

    Ghost spell added

---
+++ Assets/Scripts/Abilities/Ghost.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class Ghost : Spell
+{
+    private float radius = 20f;
+    public LayerMask StickyTarget;
+    public LayerMask WallTarget;
+    private Collider[] StickyCheck;
+    private Collider[] WallCheck;
+    
+
+    // Start is called before the first frame update
+    public override void Activate(GameObject parent)
+    {
+        Player player = parent.GetComponent<Player>();
+        StickyCheck = Physics.OverlapSphere(player.transform.position, radius, StickyTarget);
+        WallCheck = Physics.OverlapSphere(player.transform.position, radius, WallTarget);
+        if (StickyCheck.Length != 0)
+        {
+            for(int i=0; i < StickyCheck.Length; i++)
+            {
+                Debug.Log("Sticky wall here");
+                StickyCheck[i].isTrigger = true;
+  
+            }
+        }
+        if (WallCheck.Length != 0)
+        {
+            for(int i=0; i < WallCheck.Length; i++)
+            {
+                GameObject p = WallCheck[i].gameObject;
+                if (p.transform.localScale.y > p.transform.localScale.x)
+                {
+                    Debug.Log("Wall here");
+                    WallCheck[i].isTrigger = true;
+      
+                }
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    public override void Deactivate(GameObject parent)
+    {
+        Debug.Log("Spell done");
+        for(int i = 0; i < StickyCheck.Length; i++)
+        {
+            StickyCheck[i].isTrigger = false;
+        }
+        for (int i=0;i< WallCheck.Length; i++)
+        {
+            WallCheck[i].isTrigger = false;
+        }
+    }
+}
