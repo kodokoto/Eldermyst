482aa8e73b7944a5ccb18f3ae32dfeca8eab123d
jjasici1 <ec21744@qmul.ac.uk>
2023-12-15 12:21:44 +0000

    grapple animation

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -7,6 +7,8 @@ public class AnimationController : MonoBehaviour
     Animator animator;
     int isRunningHash;
     int isJumpingHash;
+    int isMagicHash;
+    int isGrappleHash;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +16,8 @@ public class AnimationController : MonoBehaviour
         animator = GetComponent<Animator>();
         isRunningHash = Animator.StringToHash("isRunning");
         isJumpingHash = Animator.StringToHash("isJumping");
+        isMagicHash = Animator.StringToHash("isMagic");
+        isGrappleHash = Animator.StringToHash("isGrapple");
     }
 
     // Update is called once per frame
@@ -23,24 +27,43 @@ public class AnimationController : MonoBehaviour
         {
             animator.SetBool(isRunningHash, false);
             animator.SetBool(isJumpingHash, true);
+            animator.SetBool(isMagicHash, false);
         }
 
-        if ((Input.GetKey("a") || Input.GetKey("d")) && (!Input.GetKey("space")))
+        if ((Input.GetKey("a") || Input.GetKey("d")) && (!Input.GetKey("space")) && !(Input.GetKey("e") || Input.GetKey("c") | Input.GetKey("r") || Input.GetKey("x") || Input.GetKey("q")))
         {
             animator.SetBool(isRunningHash, true);
             animator.SetBool(isJumpingHash, false);
+            animator.SetBool(isMagicHash, false);
+
         }
 
-        if ((!Input.GetKey("a")) && (!Input.GetKey("d")) && (!Input.GetKey("space")))
+        if ((!Input.GetKey("a")) && (!Input.GetKey("d")) && (!Input.GetKey("space")) && !(Input.GetKey("e") || Input.GetKey("c") | Input.GetKey("r") || Input.GetKey("x") || Input.GetKey("q")))
         {
             animator.SetBool(isRunningHash, false);
             animator.SetBool(isJumpingHash, false);
+            animator.SetBool(isMagicHash, false);
         }
 
-        if ((!Input.GetKey("a")) && (!Input.GetKey("d"))&& (Input.GetKey("space")))
+        if ((!Input.GetKey("a")) && (!Input.GetKey("d"))&& (Input.GetKey("space")) && !(Input.GetKey("e") || Input.GetKey("c") | Input.GetKey("r") || Input.GetKey("x") || Input.GetKey("q")))
         {
             animator.SetBool(isRunningHash, false);
             animator.SetBool(isJumpingHash, true);
+            animator.SetBool(isMagicHash, false);
+
+        }
+
+        if(Input.GetKey("e")|| Input.GetKey("c")| Input.GetKey("r")|| Input.GetKey("x")|| Input.GetKey("q"))
+        {
+            animator.SetBool(isRunningHash, false);
+            animator.SetBool(isJumpingHash, false);
+            animator.SetBool(isMagicHash, true);
+        }
+
+        //testing grapple
+        if (Input.GetKey("g"))
+        {
+            animator.SetBool(isGrappleHash, true);
         }
     }
 }
