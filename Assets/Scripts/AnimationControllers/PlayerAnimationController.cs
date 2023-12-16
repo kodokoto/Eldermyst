85e5e350ea02f4daaf9b7971b9770d399ebe9d08
modii <modiibeats@gmail.com>
2023-12-16 14:49:16 +0000

    revamp

--- Assets/Scripts/AnimationControllers/PlayerAnimationController.cs
+++ Assets/Scripts/AnimationControllers/PlayerAnimationController.cs
@@ -2,26 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+enum AnimationState {
+    Running,
+    Jumping,
+    SpellCasting,
+    Grappling,
+    Falling,
+    Idle
+}
+
 public class PlayerAnimationController : MonoBehaviour
 {
     Animator animator;
-    int isRunningHash;
-    int isJumpingHash;
-    int isMagicHash;
-    int isGrappleHash;
-    int isFallingHash;
     PlayerMovement movement;
+
+    // need to do this because there is a bug in C# regarding enum properties recursivelly calling set
+    // causing the stack to overflow and the program to crash
+    private AnimationState _animationState { get; set;}
+
+    AnimationState animationState {
+        get { return _animationState; }
+        set {
+            foreach (AnimatorControllerParameter p in animator.parameters)
+            {
+                if (p.name == "" + value)
+                {
+                    animator.SetBool(p.name, true);
+                }
+                else
+                {
+                    animator.SetBool(p.name, false);
+                }
+            }
+            _animationState = value;
+        }
+    }
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         movement = GetComponent<PlayerMovement>();
-        isRunningHash = Animator.StringToHash("isRunning");
-        isJumpingHash = Animator.StringToHash("isJumping");
-        isMagicHash = Animator.StringToHash("isMagic");
-        isGrappleHash = Animator.StringToHash("isGrappling");
-        isFallingHash = Animator.StringToHash("isFalling");
     }
 
     // Update is called once per frame
@@ -30,73 +54,43 @@ public class PlayerAnimationController : MonoBehaviour
         // if running and jumping, jump
         if ((Input.GetKey("a") || Input.GetKey("d")) && (Input.GetKey("space")))
         {
-            animator.SetBool(isRunningHash, false);
-            animator.SetBool(isJumpingHash, true);
-            animator.SetBool(isMagicHash, false);
-            animator.SetBool(isFallingHash, false);
-
+            animationState = AnimationState.Jumping;
         }
 
         // running
         if ((Input.GetKey("a") || Input.GetKey("d")) && (!Input.GetKey("space")) && !(Input.GetKey("e") || Input.GetKey("c") | Input.GetKey("r") || Input.GetKey("x") || Input.GetKey("q")))
         {
-            animator.SetBool(isRunningHash, true);
-            animator.SetBool(isJumpingHash, false);
-            animator.SetBool(isMagicHash, false);
-            animator.SetBool(isGrappleHash, false);
-            animator.SetBool(isFallingHash, false);
-
+            animationState = AnimationState.Running;
         }
 
         // idle
         if ((!Input.GetKey("a")) && (!Input.GetKey("d")) && (!Input.GetKey("space")) && !(Input.GetKey("e") || Input.GetKey("c") | Input.GetKey("r") || Input.GetKey("x") || Input.GetKey("q")))
         {
-            animator.SetBool(isRunningHash, false);
-            animator.SetBool(isJumpingHash, false);
-            animator.SetBool(isMagicHash, false);
-            animator.SetBool(isGrappleHash, false);
-            animator.SetBool(isFallingHash, false);
+            animationState = AnimationState.Idle;
         }
 
         // jumping
         if ((!Input.GetKey("a")) && (!Input.GetKey("d"))&& (Input.GetKey("space")) && !(Input.GetKey("e") || Input.GetKey("c") | Input.GetKey("r") || Input.GetKey("x") || Input.GetKey("q")))
         {
-            animator.SetBool(isRunningHash, false);
-            animator.SetBool(isJumpingHash, true);
-            animator.SetBool(isMagicHash, false);
-            animator.SetBool(isGrappleHash, false);
-            animator.SetBool(isFallingHash, false);
-
+            animationState = AnimationState.Jumping;
         }
 
         // spellcasting
         if(Input.GetKey("e")|| Input.GetKey("c")| Input.GetKey("r")|| Input.GetKey("x")|| Input.GetKey("q"))
         {
-            animator.SetBool(isRunningHash, false);
-            animator.SetBool(isJumpingHash, false);
-            animator.SetBool(isMagicHash, true);
-            animator.SetBool(isGrappleHash, false);
-            animator.SetBool(isFallingHash, false);
+            animationState = AnimationState.SpellCasting;
         }
 
         // Wall jumping
         if ((movement.wallSliding || movement.wallJumping)||(Input.GetKey("space") && (movement.touchingWallL||movement.touchingWallR)))
         {
-            animator.SetBool(isRunningHash, false);
-            animator.SetBool(isJumpingHash, false);
-            animator.SetBool(isMagicHash, false);
-            animator.SetBool(isGrappleHash, true);
-            animator.SetBool(isFallingHash, false);
+            animationState = AnimationState.Grappling;
         }
 
         // falling
         if(movement.falling && !Input.GetKey("space") &&!(movement.wallJumping||movement.wallJumping))
         {
-            animator.SetBool(isRunningHash, false);
-            animator.SetBool(isJumpingHash, false);
-            animator.SetBool(isMagicHash, false);
-            animator.SetBool(isGrappleHash, false);
-            animator.SetBool(isFallingHash, true);
+            animationState = AnimationState.Falling;
         }
     }
 }
