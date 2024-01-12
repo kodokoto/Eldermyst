57267e0fffa8d9e826eec9026399fac62c171532
modii <modiibeats@gmail.com>
2024-01-12 03:24:54 +0000

    added tutorial level + new dialogue and next level loading

--- Assets/Scripts/Events/DialogueEvent.cs
+++ Assets/Scripts/Events/DialogueEvent.cs
@@ -5,12 +5,17 @@ public class DialogueEvent : MonoBehaviour
 {
     [SerializeField] private List<string> dialogue;
     [SerializeField] private DialogueSignalSO dialogueSignal;
+    [SerializeField] private bool _destructabe = true;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             dialogueSignal.Trigger(dialogue);
+            if (_destructabe)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
