f214165af50262f5bfc019458adcc81ad7ce4ed6
jjasici1 <ec21744@qmul.ac.uk>
2024-01-12 16:34:21 +0000

    fixed spellbook

--- Assets/Scripts/Managers/SpellBookManager.cs
+++ Assets/Scripts/Managers/SpellBookManager.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using TMPro;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
@@ -12,15 +14,18 @@ public class SpellBookManager : MonoBehaviour
     public Spell PSpell;
     private int Index=0;
     [SerializeField] private SpellSignalSO spellSignal; // reference the spell signal
+    Player player;
+    List<Spell> spells;
 
     public void Start()
     {
+        player = GameObject.Find("Player").GetComponent<Player>();
+        spells = player.PlayerInventory.Spells;
         Debug.Log("Start book manager");
-        //objectToInstantiate.transform.GetChild(0).GetComponent<TextMeshPro>().text = PSpell.spellName;
-        objectToInstantiate.transform.GetChild(2).GetComponent<Image>().sprite = PSpell.icon;
-        GameObject slot =Instantiate(objectToInstantiate, spellSlots[Index].transform);
-        slot.transform.localPosition = new Vector3(-44, -475, 0);
-        Index++;
+        for(int i = 0; i < spells.Count; i++)
+        {
+            addSpellToBook(spells[i]);
+        }
         Debug.Log("Book UI should be instantiated");
     }
 
@@ -37,12 +42,44 @@ public class SpellBookManager : MonoBehaviour
     // this function gets called when the action is triggered
     public void NewSpell(Spell spell)
     {
-        // Here you have to code to add the spell to the book
-        objectToInstantiate.transform.GetChild(0).GetComponent<TextMeshPro>().text = spell.name ;
+        addSpellToBook(spell);
+        Debug.Log("Spell added to book");
+    }
 
-        //objectToInstantiate.transform.GetChild(1).GetComponent<TextMeshPro>().text = spell.Spell.combo;
-        objectToInstantiate.transform.GetChild(2).GetComponent<Image>().sprite = spell.icon;
-        Instantiate(objectToInstantiate, spellSlots[Index].transform);
+    private void addSpellToBook(Spell spell)
+    {
+        // Here you have to code to add the spell to the book
+        objectToInstantiate.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().text = spell.spellName;
+        string comboString = "";
+        comboString = ComboString(spell, comboString);
+        
+        objectToInstantiate.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().text = comboString;
+        objectToInstantiate.transform.GetChild(2).gameObject.GetComponent<Image>().sprite = spell.icon;
+        GameObject slot = Instantiate(objectToInstantiate, spellSlots[Index].transform);
+        slot.transform.localPosition = new Vector3(-44, -475, 0);
         Index++;
+    }
+
+    private string ComboString(Spell spell, string comboString)
+    {
+        foreach (SpellCombo combo in spell.combos)
+        {
+            if (comboString == "")
+            {
+                comboString += combo.keys
+                            .Select(item => (char)item)
+                            .Aggregate(new StringBuilder(), (current, next) => current.Append(next))
+                            .ToString();
+            }
+            else
+            {
+                comboString += " or " + combo.keys
+                            .Select(item => (char)item)
+                            .Aggregate(new StringBuilder(), (current, next) => current.Append(next))
+                            .ToString();
+            }
+
+        }
+        return comboString;
     }
 }
